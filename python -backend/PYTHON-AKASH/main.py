f6133848686166302316009fa82398bf425c4d4d
akash-v2725 <akash.v22td0653@svcet.ac.in>
2025-01-29 16:32:26 +0530

    Add files via upload

---
+++ python -backend/PYTHON-AKASH/main.py
@@ -0,0 +1,9 @@
+chappathy=int(input("how many chappathy you had in the lunch ?")) 
+if chappathy>=1 and chappathy<=2:
+    print("hungry")
+elif chappathy>=3 and chappathy<=5:
+    print("sleep")
+else:
+    print("fasting") 
+
+ 
